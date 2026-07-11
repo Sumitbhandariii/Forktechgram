ac6d7c9baa7e79df1f81cae13649b536d11fab29
Sumitbhandariii <86198377+Sumitbhandariii@users.noreply.github.com>
2026-07-11 17:01:53 +0530

    Update SettingsActivity.java
    [skip ci]

--- TMessagesProj/src/main/java/org/telegram/ui/SettingsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/SettingsActivity.java
@@ -689,6 +689,13 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
             items.add(UItem.asHeader("Nova Power"));
             items.add(SettingCell.Factory.of(98, IconBackgroundColors.PURPLE.top, IconBackgroundColors.PURPLE.bottom, R.drawable.settings_fork, getString(R.string.ForkSettingsTitle)));
             items.add(SettingCell.Factory.of(99, IconBackgroundColors.BLUE_DEEP.top, IconBackgroundColors.BLUE_DEEP.bottom, R.drawable.settings_check_update, getString(R.string.ForkCheckUpdate)));
+            FrameLayout adContainer = new FrameLayout(getParentActivity());
+            com.google.android.gms.ads.AdView adView = new com.google.android.gms.ads.AdView(getParentActivity());
+            adView.setAdSize(com.google.android.gms.ads.AdSize.BANNER);
+            adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+            adContainer.addView(adView);
+            adView.loadAd(new com.google.android.gms.ads.AdRequest.Builder().build());
+            items.add(UItem.asCustom(adContainer));
         }
 
         items.add(UItem.asShadow(null));
@@ -734,13 +741,6 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
         items.add(SettingCell.Factory.of(18, IconBackgroundColors.BLUE_LIGHT.top, IconBackgroundColors.BLUE_LIGHT.bottom, R.drawable.settings_faq, getString(R.string.TelegramFAQ)));
         items.add(SettingCell.Factory.of(23, IconBackgroundColors.PURPLE.top, IconBackgroundColors.PURPLE.bottom, R.drawable.settings_features, getString(R.string.TelegramFeatures)));
         items.add(SettingCell.Factory.of(19, IconBackgroundColors.GREEN.top, IconBackgroundColors.GREEN.bottom, R.drawable.settings_policy, getString(R.string.PrivacyPolicy)));
-        FrameLayout adContainer = new FrameLayout(getParentActivity());
-        com.google.android.gms.ads.AdView adView = new com.google.android.gms.ads.AdView(getParentActivity());
-        adView.setAdSize(com.google.android.gms.ads.AdSize.BANNER);
-        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
-        adContainer.addView(adView);
-        adView.loadAd(new com.google.android.gms.ads.AdRequest.Builder().build());
-        items.add(UItem.asCustom(adContainer));
 
         if (BuildVars.LOGS_ENABLED || BuildVars.DEBUG_PRIVATE_VERSION) {
             items.add(UItem.asShadow(null));
