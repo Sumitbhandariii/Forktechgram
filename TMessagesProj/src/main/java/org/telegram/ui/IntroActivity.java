21d0e7357f0d112152fa77bf41190ea7df01e06c
Sumitbhandariii <86198377+Sumitbhandariii@users.noreply.github.com>
2026-07-18 18:13:23 +0530

    Add ImageView import to IntroActivity

--- TMessagesProj/src/main/java/org/telegram/ui/IntroActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/IntroActivity.java
@@ -47,6 +47,7 @@ import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.core.graphics.ColorUtils;
